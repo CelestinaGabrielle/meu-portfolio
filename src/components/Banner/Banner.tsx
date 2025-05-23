40dce2e8d362b8b060d9fc8a456cd565f0ba4e2a
Gabrielle_CA <gabriellecelestinaa@gmail.com>
2025-05-23 13:52:44 -0300

    Atualizando o curriculo

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -5,7 +5,7 @@ import minhaFoto from "../../assets/images/profile-pic.png";
 const Banner: React.FC = () => {
   const handleDownloadCV = () => {
     const link = document.createElement("a");
-    link.href = "/Curriculo_Gabrielle_Celestina.pdf"; // Caminho correto para o arquivo na pasta public
+    link.href = "/Curriculo_Gabrielle_Celestina.pdf";
     link.download = "Curriculo_Gabrielle_Celestina.pdf";
     link.click();
   };
